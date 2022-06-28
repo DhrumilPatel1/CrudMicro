02c1344a648af54267bcafacb42d37cc90cb5277
arcus_nodejs_dev <abhishek.patel@arcuspartner.com>
2022-06-28 18:17:36 +0530

    merge

---
+++ UpdateTodo/config/db.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+module.exports.mongoDBClientConnect = async function mongoDBClientConnect()
+{
+    const uri = 'mongodb+srv://dhrumil:dhrumil%40998@cluster0.blxmx.mongodb.net/test';
+    try 
+    {
+        mongoose.connect(
+            uri,
+            { useNewUrlParser: true,useUnifiedTopology: true},
+            function (err, db) {
+              if (err) {
+                console.log("MongoDB Database Connection Error", err);
+              } else {
+                console.log("MongoDB Connection Done!!");
+              }
+            }
+          );
+    }
+    catch (error) 
+    {
+        console.log("mongoDBClientConnect: Database Error : " + error);
+        throw error;
+    }
+}
--- UpdateTodo/index.js
+++ UpdateTodo/index.js
@@ -1,4 +1,6 @@
+const { mongoDBClientConnect } = require("./config/db");
 const httpResponse = require("./config/HttpResponse");
 exports.handler = async (event, context) => {
+  await mongoDBClientConnect()
   return httpResponse.HttpResponse(200, "my_value");
 };
