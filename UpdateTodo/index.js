20153a93a4faa6172880f13710040f710f5963d0
arcus_nodejs_dev <abhishek.patel@arcuspartner.com>
2022-06-28 14:36:22 +0530

    merge

--- UpdateTodo/index.js
+++ UpdateTodo/index.js
@@ -7,7 +7,7 @@ module.exports.handler = async (req, res) => {
     httpResponse(200, obj);
   } catch (error) {
     obj = {
-      message: "Intsernal Server Error",
+      message: "Intsessrnal Server Error",
     };
     httpResponse(500, obj);
   }
