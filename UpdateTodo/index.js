7b54caeb20fc1dbbfb14caf8cfdc83097d7e1e1e
arcus_nodejs_dev <abhishek.patel@arcuspartner.com>
2022-06-28 17:50:53 +0530

    merge

--- UpdateTodo/index.js
+++ UpdateTodo/index.js
@@ -1,20 +1,10 @@
 const HttpResponse = require("./config/HttpResponse");
 module.exports.handler =async function handler() {
-  try {
     return HttpResponse(200,"my_value")
-  } catch (error) {
-    return {
-      "statusCode": 500,
-      "headers": {
-          "my_header": "my_value"
-      },
-      "body": JSON.stringify({"A":"SERVER ERROR"}),
-      "isBase64Encoded": false
-  };
+ 
   }
-  console.log("I asassssssddddsadasdddaaM  Dhrsadfgdsasumil Patel");
 
   
-}
+
 
 
