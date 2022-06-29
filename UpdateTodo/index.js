26291ed5d9f206c396bf36692adcfccc49075e02
arcus_nodejs_dev <abhishek.patel@arcuspartner.com>
2022-06-29 11:04:41 +0530

    merge

--- UpdateTodo/index.js
+++ UpdateTodo/index.js
@@ -2,6 +2,7 @@ const { mongoDBClientConnect } = require("./config/db");
 const httpResponse = require("./config/HttpResponse");
 const TodoModel = require("./Model/Todo");
 exports.handler = async (event, context) => {
+  console.log("EVENT",event,"AAA");
   await mongoDBClientConnect()
   var create = await TodoModel.create({title:"dd",descriptions:"descriptions"})
   return httpResponse.HttpResponse(200, "my_value");
