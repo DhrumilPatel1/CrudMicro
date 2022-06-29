4a197092b673dce8482ddaaa8174633afe9b7a57
arcus_nodejs_dev <abhishek.patel@arcuspartner.com>
2022-06-29 12:00:58 +0530

    merge

--- UpdateTodo/index.js
+++ UpdateTodo/index.js
@@ -5,8 +5,9 @@ const isEmpty = require("./validation/is-empty");
 const validateCreateTodo = require("./validation/validationCreateTodo");
 exports.handler = async (event, context) => {
   try {
+    let body = JSON.parse(event.body);
     console.log("EVENT", event, "AAA");
-    var { errors, isValid } = validateCreateTodo(event);
+    var { errors, isValid } = validateCreateTodo(body);
     if (!isEmpty(errors)) {
       return httpResponse.HttpResponse(422, "my_value", {
         message: "missing somefield",
@@ -14,7 +15,7 @@ exports.handler = async (event, context) => {
       });
     } else {
       await mongoDBClientConnect();
-      var create = await TodoModel.create(event);
+      var create = await TodoModel.create(body);
       return httpResponse.HttpResponse(200, "my_value", {
         message: "Todo Created Successfully",
       });
