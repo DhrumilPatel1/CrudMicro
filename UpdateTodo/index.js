1cfc5f7232c6a99f3342a7050f720e7969c687a4
arcus_nodejs_dev <abhishek.patel@arcuspartner.com>
2022-07-04 11:28:48 +0530

    merge

--- UpdateTodo/index.js
+++ UpdateTodo/index.js
@@ -7,27 +7,27 @@ exports.handler = async (event, context) => {
   try {
     let body = JSON.parse(event.body);
     var { errors, isValid } = validateCreateTodo(body);
-    // var findId = await TodoModel.findOne({_id:body.id});
-    
-    // if (!findId) {
-    //   errors.id = "Id is invalid"
-    // }
+    await mongoDBClientConnect();
+    var findId = await TodoModel.findOne({ _id: body.id });
+
+    if (!findId) {
+      errors.id = "Id is invalid";
+    }
     if (!isEmpty(errors)) {
       return httpResponse.HttpResponse(422, {
         message: "missing somefield",
         error: errors,
       });
     } else {
-      await mongoDBClientConnect();
-      var create = await TodoModel.updateOne({_id:body.id},body);
-      return httpResponse.HttpResponse(200,  {
+      var create = await TodoModel.updateOne({ _id: body.id }, body);
+      return httpResponse.HttpResponse(200, {
         message: "Todo Updated Successfully",
       });
     }
   } catch (error) {
-    return httpResponse.HttpResponse(500,  {
+    return httpResponse.HttpResponse(500, {
       message: "Intesrnal Server Error",
-      error:error
+      error: error,
     });
   }
 };
